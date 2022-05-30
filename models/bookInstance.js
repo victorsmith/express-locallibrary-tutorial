98c0f141f11416e8ada467509c9e451f01ecaaf6
victorsmith <victor7smith@gmail.com>
2022-05-29 23:07:07 -0600

    bookInstanceDone

--- models/bookInstance.js
+++ models/bookInstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 var Schema = mongoose.Schema;
 
@@ -11,12 +12,17 @@ var BookInstanceSchema = new Schema({
 		enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'],
 		default: 'Maintenance',
 	},
+
 	due_back: { type: Date, default: Date.now },
 });
 
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual('url').get(function () {
 	return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+	return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
